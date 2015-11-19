8ecfc2e36d720272b741a1511259d6ee0c72a69d
Charlie Chan <charliecyh95@gmail.com>
2015-11-20 10:43:48 +1100

    Using Layouts

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,42 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome to Forster's Gym - 2015</title>
-    <link rel="stylesheet"
-          href="css/bootstrap.min.css">
-    <link rel="stylesheet"
-          href="css/bootstrap-cosmo.min.css">
+@extends('layouts.master')
 
+@section('styles')
     <link rel="stylesheet"
           href="css/mystyles.css">
-</head>
+@endsection
 
-<body>
-    <!-- navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a href="index.php" class="navbar-brand">Forster's Gym 2015</a>
-            </div>
-
-            <div class="navbar-collapse" id="navbar-main">
-                <ul class="nav navbar-nav navbar-right">
-                    <p class="navbar-text navbar-right">Coded by CharlieC - 2015 </p>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container jumbotron-page">
-        <div class="jumbotron">
-            <h2>Welcome to Forster's Gym 2015</h2>
-            <p>Enjoy using Laravel 5.1</p>
-            <p><a class="btn btn-primary btn-sm"
-                  href="#" role="button">Click here to start</a>
-            </p>
-        </div>
-    </div>
-
-    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+@section('content')
+    @include('pages.partials.home-nav-bar')
+    @include('pages.partials.home-jumbotron')
+@endsection
---
+++ resources/views/pages/partials/home-nav-bar.blade.php
@@ -0,0 +1,14 @@
+<!-- navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <p href="index.php" class="navbar-brand">Forster's Gym 2015</p>
+        </div>
+
+        <div class="navbar-collapse" id="navbar-main">
+            <ul class="nav navbar-nav navbar-right">
+                <p class="navbar-text navbar-right">Coded by {{ config('forstersgym.coder') }} </p>
+            </ul>
+        </div>
+    </div>
+</nav>
